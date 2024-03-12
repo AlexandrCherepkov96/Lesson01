a291a10c03db4e26efe0e660d0a81c2f3e56ee6a
Alexandr Cherepkov <sasacherepkov@gmail.com>
2024-03-13 04:42:28 +0500

    add Lesson01:Task4

---
+++ Task4/Program.cs
@@ -0,0 +1,25 @@
+﻿int a = 30;
+int b = 25;
+int c = 9;
+int d = 15;
+int e = 6;
+
+int max = a;
+
+if (b > max)
+{
+    max = b;
+}
+if (c > max)
+{
+    max = c;
+}
+if (d > max)
+{
+    max = d;
+}
+if (e > max)
+{
+    max = e;
+}
+Console.WriteLine(max);
