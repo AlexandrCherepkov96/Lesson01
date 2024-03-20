9eb7e3efc431cdd2553ef4b35bed083b4494f119
Alexandr Cherepkov <sasacherepkov@gmail.com>
2024-03-20 20:17:45 +0500

    Добавили еще несколько задач

---
+++ Lesson02/Task3/Program.cs
@@ -0,0 +1,12 @@
+﻿int n = 10;
+int[] array = {1, 5, 10, 20, 30, 40, 99, 4, 90, 3};
+int i = 0;
+
+while(i < n)
+{
+    if(array[i] % 10 == 0 )
+    {
+        Console.Write($"{array[i]} ");
+    }
+    i = i + 1;
+}
