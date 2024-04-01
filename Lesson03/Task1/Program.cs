3e13322bd3e5b7a707419d13f922c38322b859b4
Alexandr Cherepkov <sasacherepkov@gmail.com>
2024-04-01 15:39:18 +0500

    add lesson3: Task1

---
+++ Lesson03/Task1/Program.cs
@@ -0,0 +1,9 @@
+﻿double CalculateFormula(int a, int b, int c, int d)
+{
+    int numerator = a * b;
+    int denomenator = c + d;
+    double result = numerator / denomenator;
+    return result;
+}
+double result = CalculateFormula(1, 2, 3, 4);
+Console.WriteLine(result);
